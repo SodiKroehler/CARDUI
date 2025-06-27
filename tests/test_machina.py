6d20e32081e2b7a3910db37210eb912369e9ff92
Sodi Kroehler <sodi@chitters.net>
2025-06-26 21:08:56 -0400

    final v0.1.6 commit - adding envs in publisher

--- tests/test_machina.py
+++ tests/test_machina.py
@@ -1,23 +1,29 @@
 import unittest
 from CARDUI import Machina, Structura
+import os
+import pytest
 
 class test_machina_runs(unittest.TestCase):
 
+    @pytest.mark.skipif("OPENAI_API_KEY" not in os.environ, reason="Missing OpenAI API key")
     def test_openai_auth(self):
         machina = Machina(model_provider="OpenAI", model_name="gpt-4o")
         status = machina.auth()
         self.assertEqual(status, "Authentication Success")
 
+    @pytest.mark.skipif("ANTHROPIC_API_KEY" not in os.environ, reason="Missing Anthropic API key")
     def test_anthropic_auth(self):
         machina = Machina(model_provider="Anthropic", model_name="claude-2")
         status = machina.auth()
         self.assertEqual(status, "Authentication Success")
     
+    @pytest.mark.skipif("MISTRAL_API_KEY" not in os.environ, reason="Missing OpenAI API key")
     def test_mistral_auth(self):
         machina = Machina(model_provider="Mistral", model_name="mistral-large")
         status = machina.auth()
         self.assertEqual(status, "Authentication Success")
     
+    @pytest.mark.skipif("GOOGLE_API_KEY" not in os.environ, reason="Missing OpenAI API key")
     def test_google_auth(self):
         machina = Machina(model_provider="Google", model_name="gemini-1.5-pro")
         status = machina.auth()

6f0823cca9632595d225380cf99ddd77d042de1b
Sodi Kroehler <sodi@chitters.net>
2025-06-26 21:19:52 -0400

    final v0.1.7 commit - added env var logging

--- tests/test_machina.py
+++ tests/test_machina.py
@@ -7,6 +7,7 @@ class test_machina_runs(unittest.TestCase):
 
     @pytest.mark.skipif("OPENAI_API_KEY" not in os.environ, reason="Missing OpenAI API key")
     def test_openai_auth(self):
+        print(f"[TEST] OPENAI_API_KEY: {os.getenv('OPENAI_API_KEY')}")
         machina = Machina(model_provider="OpenAI", model_name="gpt-4o")
         status = machina.auth()
         self.assertEqual(status, "Authentication Success")
@@ -14,18 +15,21 @@ class test_machina_runs(unittest.TestCase):
     @pytest.mark.skipif("ANTHROPIC_API_KEY" not in os.environ, reason="Missing Anthropic API key")
     def test_anthropic_auth(self):
         machina = Machina(model_provider="Anthropic", model_name="claude-2")
+        print(f"[TEST] ANTHROPIC_API_KEY: {os.getenv('ANTHROPIC_API_KEY')}")
         status = machina.auth()
         self.assertEqual(status, "Authentication Success")
-    
-    @pytest.mark.skipif("MISTRAL_API_KEY" not in os.environ, reason="Missing OpenAI API key")
+
+    @pytest.mark.skipif("MISTRAL_API_KEY" not in os.environ, reason="Missing Mistral API key")
     def test_mistral_auth(self):
         machina = Machina(model_provider="Mistral", model_name="mistral-large")
+        print(f"[TEST] MISTRAL_API_KEY: {os.getenv('MISTRAL_API_KEY')}")
         status = machina.auth()
         self.assertEqual(status, "Authentication Success")
-    
-    @pytest.mark.skipif("GOOGLE_API_KEY" not in os.environ, reason="Missing OpenAI API key")
+
+    @pytest.mark.skipif("GOOGLE_API_KEY" not in os.environ, reason="Missing Google API key")
     def test_google_auth(self):
         machina = Machina(model_provider="Google", model_name="gemini-1.5-pro")
+        print(f"[TEST] GOOGLE_API_KEY: {os.getenv('GOOGLE_API_KEY')}")
         status = machina.auth()
         self.assertEqual(status, "Authentication Success")
 
